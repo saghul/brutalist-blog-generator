f25d9277ccf271bf69d462864d3a33c8728fea77
Saúl Ibarra Corretgé <s@saghul.net>
2025-03-17 10:20:28 +0100

    Process all documents in the source directory and generate output
    It's not templatized yet.

--- Sources/Commands/BuildCommand.swift
+++ Sources/Commands/BuildCommand.swift
@@ -10,26 +10,48 @@ struct BuildCommand: ParsableCommand {
     func run() throws {
         print("Building website...")
 
-        do {
-            let filePath = "README.md"
+        let fileManager = FileManager.default
+        let directoryURL = URL(fileURLWithPath: "www/posts")
 
-            // Parse document
-            let document = try Document.parse(path: filePath)
-
-            // Convert to HTML
-            let html = document.toHtml()
-
-            // Save to file
-            let outputPath = filePath.replacingOccurrences(of: ".md", with: ".html")
-            try html.write(toFile: outputPath, atomically: true, encoding: .utf8)
-
-            print("Successfully generated HTML at: \(outputPath)")
-            print("Title: \(document.title)")
-            print("Date: \(document.date)")
-            print("Slug: \(document.slug)")
-        } catch {
-            print("Error: \(error)")
-            throw error
+        if let enumerator = fileManager.enumerator(atPath: directoryURL.path) {
+            while let filePath = enumerator.nextObject() as? String {
+                if filePath.hasSuffix(".md") {
+                    let fullPathUrl = directoryURL.appendingPathComponent(filePath)
+                    print("Found file: \(fullPathUrl.path)")
+                    do {
+                        try processFile(at: fullPathUrl.path)
+                    } catch {
+                        print("Failed to process file: \(fullPathUrl.path)")
+                        print(error)
+                    }
+                }
+            }
         }
+    }
+
+    private func processFile(at path: String) throws {
+        let document = try Document.parse(path: path)
+        //print("Title: \(document.title)")
+        //print("Date: \(document.date)")
+        //print("Slug: \(document.slug)")
+
+        let directoryURL = URL(fileURLWithPath: "build")
+
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: document.date)
+        let month = calendar.component(.month, from: document.date)
+        let outputDirUrl = directoryURL
+            .appendingPathComponent(String(format: "%04d", year))
+            .appendingPathComponent(String(format: "%02d", month))
+        let fileManager = FileManager.default
+        try fileManager.createDirectory(at: outputDirUrl, withIntermediateDirectories: true, attributes: nil)
+        let outputUrl = outputDirUrl
+            .appendingPathComponent(document.slug)
+            .appendingPathExtension("html")
+
+        let html = document.toHtml()
+        try html.write(toFile: outputUrl.path, atomically: true, encoding: .utf8)
+
+        print("Successfully generated HTML at: \(outputUrl.path)")
     }
 }
