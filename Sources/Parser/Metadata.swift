9cb4c08a98bf499f5a74bce38e1f23f5c10e6114
Saúl Ibarra Corretgé <s@saghul.net>
2025-03-18 08:57:07 +0100

    Make date mandatory for posts

--- Sources/Parser/Metadata.swift
+++ Sources/Parser/Metadata.swift
@@ -1,26 +1,39 @@
 import Foundation
 import Yams
 
+enum MetadataError: Error {
+    case invalidDate
+    case missingDate
+}
+
 public struct DocumentMetadata {
-    let title: String?
     let date: Date
+    let title: String?
     let slug: String?
 
-    private init(title: String?, date: Date, slug: String?) {
-        self.title = title
+    private init(date: Date, title: String?, slug: String?) {
         self.date = date
+        self.title = title
         self.slug = slug
     }
 
     public static func parse(data: String?) throws -> DocumentMetadata {
         let yaml = try Yams.load(yaml: data ?? "") as? [String: String]
 
+        // Extract date and make sure it's correctly formatted'
+        let dateStr = yaml?["date"]
+        guard let validDateStr = dateStr else {
+            throw MetadataError.missingDate
+        }
+        let formatter = ISO8601DateFormatter()
+        let date = formatter.date(from: validDateStr)
+        guard let validDate = date else {
+            throw MetadataError.invalidDate
+        }
+
         let title = yaml?["title"]
         let slug = yaml?["slug"]
-        let dateStr = yaml?["date"] ?? ""
-        let formatter = ISO8601DateFormatter()
-        let date = formatter.date(from: dateStr) ?? Date()
 
-        return DocumentMetadata(title: title, date: date, slug: slug)
+        return DocumentMetadata(date: validDate, title: title, slug: slug)
      }
 }
