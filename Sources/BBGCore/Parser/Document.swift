d893838c880e6194e73f7f3f6c9d350e98811a87
Saúl Ibarra Corretgé <s@saghul.net>
2025-04-20 10:46:53 +0200

    Add Document tests

--- Sources/BBGCore/Parser/Document.swift
+++ Sources/BBGCore/Parser/Document.swift
@@ -104,7 +104,7 @@ extension Document {
         var result: String? = nil
         if let latin = string.applyingTransform(StringTransform("Any-Latin; Latin-ASCII; Lower;"), reverse: false) {
             let urlComponents = latin.components(separatedBy: slugSafeCharacters.inverted)
-            result = urlComponents.filter { $0 != "" }.joined(separator: "-")
+            result = urlComponents.filter { $0 != "" && $0 != "-" }.joined(separator: "-")
         }
 
         guard let result = result, !result.isEmpty else {
